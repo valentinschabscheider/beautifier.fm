f6eea5717a17d96dade843c40b2f73709c3eaeed
David <david.salzmann24@gmail.com>
2020-07-22 10:38:20 +0200

    Add files via upload

---
+++ utility.js
@@ -0,0 +1,26 @@
+// Global variable section
+
+var StringConstants = {
+    Space: " ",
+    Plus: "+",
+    Slash: "/",
+};
+
+const baseUrl = "https://www.last.fm/music/";
+const baseUrlUser = "https://www.last.fm/user/";
+
+
+function GetLastFmTrackURL(artist, album, track) {
+    return baseUrl +
+        artist.replace(StringConstants.Space, StringConstants.Plus) + StringConstants.Slash +
+        album.replace(StringConstants.Space, StringConstants.Plus) + StringConstants.Slash +
+        track.replace(StringConstants.Space, StringConstants.Plus);
+}
+
+
+function GetLastFmTrackURLForUser(username, artist, album, track) {
+    return baseUrlUser + username + "/library/music/" +
+        artist.replace(StringConstants.Space, StringConstants.Plus) + StringConstants.Slash +
+        album.replace(StringConstants.Space, StringConstants.Plus) + StringConstants.Slash +
+        track.replace(StringConstants.Space, StringConstants.Plus);
+}
