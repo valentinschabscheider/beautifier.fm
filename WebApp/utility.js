21f5c9c9d786c633746c7c781e86e9fefcbdb362
test@email.com <david.salzmann24@gmail.com>
2020-12-12 12:08:09 +0100

    re-adding my temporary changes that were lost

--- WebApp/utility.js
+++ WebApp/utility.js
@@ -4,6 +4,7 @@ var StringConstants = {
     Space: " ",
     Plus: "+",
     Slash: "/",
+    Empty: ""
 };
 
 const baseUrl = "https://www.last.fm/music/";
@@ -34,5 +35,29 @@ function ShowElement(elementId) {
 
 function HideElement(elementId) {
     var element = document.getElementById(elementId);
-    element.setAttribute("hidden", "");
+    element.setAttribute("hidden", null);
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
 }
