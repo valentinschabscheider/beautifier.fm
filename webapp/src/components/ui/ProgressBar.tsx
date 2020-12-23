2ea021cac110ac6728a4e1215ec841b0fb5c3b40
test@email.com <david.salzmann24@gmail.com>
2020-12-23 14:33:43 +0100

    remove old progressbar

--- webapp/src/components/ui/ProgressBar.tsx
+++ webapp/src/components/ui/ProgressBar.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 
-import "./ProgressBar.scss";
-
 import { ProgressBar } from "react-bootstrap";
 
 interface Props {
   value: number;
-  text?: string | null;
-  css?: object;
 }
 
-const ProgressBarOwn: React.FC<Props> = ({ value, text, css }) => {
+const ProgressBarOwn: React.FC<Props> = ({ value }) => {
   return (
     <ProgressBar animated striped variant="danger" now={value} label={`${value}%`}/>
   );
